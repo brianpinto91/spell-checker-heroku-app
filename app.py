ebe3c76edab2a3918114b0193dc31db7376bfb01
brianpinto91 <brianpinto91@gmail.com>
2020-11-05 18:41:48 +0100

    implement layout and styles

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def home():
 def about():
     return flask.render_template("about.html")
 
+@app.route("/spellcheck", methods=['POST'])
+def spellcheck():
+    return "<p>working on it<p>"
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
     
